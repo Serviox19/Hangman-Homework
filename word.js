f52a673c6b5fc122ee7fbec5e8caf5584386cede
Serviox19 <servio19@gmail.com>
2016-01-26 17:56:19 -0500

    finished letter and word constructors

--- word.js
+++ word.js
@@ -3,5 +3,39 @@ var Letter = require("./letter.js");
 function word (wrd){
   this.word = wrd;
   this.lets = [];
-  this.found = false
+  this.found = false;
+  this.getLets = function(){
+    for (var i = 0; i < this.word.length; i++) {
+      var wordLetter = Letter(this.charac);
+    }
+      console.log(this.lets);
+  }
+
+  this.checkIfLetterFound = function(guessLetter){
+    var = whatToReturn = 0;
+    for (var i = 0; i < this.lets.length; i++) {
+      if (Letter.charac = guessLetter) {
+        Letter.appear = true;
+        whatToReturn = ++;
+      }
+        return whatToReturn;
+    }
+  }
+
+  this.didWeFindTheWord = function(){
+    this.lets.every = function(curLet){
+      if (curLet.appear === true) {
+        this.found = true;
+      }
+    }
+  }
+
+  this.wordRender = function(){
+    var str = "";
+    for (var i = 0; i < this.lets.length; i++) {
+      str += Letter.letterRender(this.lets[i]);
+    };
+  }
 }
+
+module.exports = Word;

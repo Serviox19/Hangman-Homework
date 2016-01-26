05d120b91c1632681a757ca253fa977435b45eed
Serviox19 <servio19@gmail.com>
2016-01-26 18:12:43 -0500

    started main.js game file

---
+++ main.js
@@ -0,0 +1,11 @@
+var prompt = require("")
+var Word = require("./word.js")
+
+game = {
+  wordBank: ["Jersey" "Android" "Millionare"];
+  guessesRemaining: 10;
+  currentWrd: null;
+  startGame: function(wrd){
+    word {}
+  };
+}
